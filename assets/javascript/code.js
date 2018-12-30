8d14d8909e2b43b8990a69e5be13a685cfd52394
Jason Kim <jasonkim4201@gmail.com>
2018-12-30 18:42:12 -0500

    have basic functionality of buttons

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,5 +1,5 @@
-// create an array of strings and save to variable called topics
-//app should take the topics in this array and create buttons in HTML. Try using a loop that appends a button for each string in the array.
+// create an array of strings and save to variable called topics. DONE
+//app should take the topics in this array and create buttons in HTML. Try using a loop that appends a button for each string in the array. DONE
 //When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
 //When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 // Under every gif, display its rating (PG, G, so on).
@@ -10,17 +10,28 @@
 var food = ["taco", "pancakes", "pizza"];
 console.log(food);
 renderButtons();
+
 //search food button
 $("#searchBtn").on("click", function(event) {
-
 event.preventDefault();
 
 var foodSearched = $("#foodInput").val().trim();
+if (foodSearched === "") {
+  false;
+} else {
 food.push(foodSearched);
 console.log(food);
 renderButtons();
+}
 });
 
+//clear button
+$("#default").on("click", function() {
+  /* location.reload(); */
+  $("#foodButtonArea").empty();
+  $("#foodGifArea").empty();
+  food.length = 0;
+});
 
 //rendered buttons function
 function renderButtons() {
@@ -28,15 +39,39 @@ function renderButtons() {
 
   for (var i = 0; i < food.length; i++) {
     var foodButton = $("<button>");
-    foodButton.addClass("btn btn-primary btn-lg");
+    foodButton.addClass("btn btn-primary btn-lg gifBtn");
     foodButton.attr("data-name", food[i]);
     foodButton.text(food[i]);
     $("#foodButtonArea").append(foodButton);
   }
 }
 
+//displayFood function
+function displayFood() {
+  var foodType = $(this).attr("data-name");
+  var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + foodType + "&api_key=RLHE1XgeOLJ2GDhYIp7941B9xDiXzHdf&limit=10";
+  
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var results = response.data;
 
+    for (var j = 0; j < results.length; j++) {
+      var gifDiv = $("<div>");
+      var p = $("<p>");
+      p.text(results[j].rating);
+      var foodImage = $("<img>");
+      foodImage.attr("src", results[j].images.original_still.url);
+      gifDiv.append(p);
+      gifDiv.append(foodImage);
+      $("#foodGifArea").prepend(gifDiv);
+    }
+    
 
+  });
+  
+}
 
-
-
+$(document).on("click", ".gifBtn", displayFood);
