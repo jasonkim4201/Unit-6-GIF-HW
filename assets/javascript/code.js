1fff4a79eccde51efa5849729056341e3e9434de
Jason Kim <jasonkim4201@gmail.com>
2018-12-31 13:46:40 -0500

    added instructions so users will know to click on gifs

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -69,6 +69,10 @@ function displayFood() {
       $("#foodGifArea").prepend(gifDiv);
     }
   });  
+
+  $("#gifInstruction").addClass("alert alert-warning text-center text-dark");
+  $("#gifInstruction").attr("role", "alert");
+  $("#gifInstruction").text("🥙🍗🥖Click on the gifs to see the magic!🥐🍖🥗"); 
 }
 
 //When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
