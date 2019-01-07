ef0d51f8a7fd8194d14bed918de6af896b6488e8
Jason Kim <jasonkim4201@gmail.com>
2019-01-07 09:23:20 -0500

    added readMe content and removed some food emojis to make the web space a bit more mobile friendly :(

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Unit-6-GIF-HW
+
+This is a readME!
+
+Type in any food item into the search bar and click on the imgages to animate the gifs!
+
+Have fun searing all the wonderful gifs to your hearts content!
--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -74,7 +74,7 @@ function displayFood() {
 
   $("#gifInstruction").addClass("alert alert-warning text-center text-dark");
   $("#gifInstruction").attr("role", "alert");
-  $("#gifInstruction").text("🥙🍗🥖Click on the gifs to see the magic!🥐🍖🥗"); 
+  $("#gifInstruction").text("Click on the gifs to see the magic!"); 
 }
 
 //When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
